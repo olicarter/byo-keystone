5ae0da2612a9bb51636470b3ce420c440da5719e
Oli Carter <olicartr@gmail.com>
2020-10-21 14:47:10 +0300

    more settings

--- lists/Setting.js
+++ lists/Setting.js
@@ -1,3 +1,4 @@
+const { Integer, Text, Url } = require('@keystonejs/fields');
 const { Markdown } = require('@keystonejs/fields-markdown');
 
 module.exports = {
@@ -11,6 +12,30 @@ module.exports = {
     homeContent: {
       type: Markdown,
       isRequired: true,
+    },
+    aboutContent: {
+      type: Markdown,
+      isRequired: true,
+    },
+    facebookUrl: {
+      type: Url,
+      isRequired: true,
+    },
+    instagramUrl: {
+      type: Url,
+      isRequired: true,
+    },
+    minOrderValue: {
+      type: Integer,
+      defaultValue: 20,
+    },
+    chooseDeliverySlotInfo: {
+      type: Text,
+      isMultiline: true,
+    },
+    orderSubmissionInfo: {
+      type: Text,
+      isMultiline: true,
     },
   },
 };
