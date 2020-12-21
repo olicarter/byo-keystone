c9f3fbb6d836b86a886b2231fbe69cb6758aeadc
Oli Carter <olicartr@gmail.com>
2020-12-21 09:30:49 +0200

    product images

--- lists/Product.js
+++ lists/Product.js
@@ -1,8 +1,18 @@
 const { Relationship, Slug, Text } = require('@keystonejs/fields');
 const { atTracking, byTracking, logging } = require('@keystonejs/list-plugins');
+const { CloudinaryAdapter } = require('@keystonejs/file-adapters');
+const { CloudinaryImage } = require('@keystonejs/fields-cloudinary-image');
+
+const fileAdapter = new CloudinaryAdapter({
+  cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  apiKey: process.env.CLOUDINARY_KEY,
+  apiSecret: process.env.CLOUDINARY_SECRET,
+  folder: 'my-keystone-app',
+});
 
 module.exports = {
   fields: {
+    image: { type: CloudinaryImage, adapter: fileAdapter },
     name: { type: Text, isRequired: true, isUnique: true },
     slug: { type: Slug },
     category: {
